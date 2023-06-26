7f87b44948f02f45cf446b2355dd0f19dbb191f5
shinhyeji <shinhyeiji@gmail.com>
2023-06-26 23:07:08 +0900

    profilecard

---
+++ index.js
@@ -0,0 +1,11 @@
+const others = document.querySelector(".others")
+const on = document.querySelector(".on")
+const button = document.querySelector("a")
+const flip = document.querySelector("flip")
+
+button.addEventListener("click", (e) => {
+        e.location(".on")
+}
+)
+
+
